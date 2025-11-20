5345a6825fce9f06ba12a7274a5aa12745a568ce
Julián Calderón Almendros <earendil.elrond.arwen@gmail.com>
2025-11-20 21:29:52 +0100

    Actualizar las rutas de inclusión en CMakeLists.txt y test_primes_compiletime_catch2.cpp para mejorar la organización de los archivos de prueba

--- tests/test_primes_compiletime_catch2.cpp
+++ tests/test_primes_compiletime_catch2.cpp
@@ -1,5 +1,5 @@
 #define CATCH_CONFIG_MAIN
-#include <catch2/catch.hpp>
+#include "external/catch.hpp"
 #include "../include/core/internal/lookup_tables/primes_compiletime.hpp"
 
 TEST_CASE("Primalidad compile-time y runtime", "[primes]")
