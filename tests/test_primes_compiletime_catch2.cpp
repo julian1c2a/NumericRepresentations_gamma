18ada380b28c8d1fcf7535f5b9bb85bc0ac69fd8
Julián Calderón Almendros <earendil.elrond.arwen@gmail.com>
2025-11-20 21:22:12 +0100

    Agregar pruebas de primalidad compile-time y runtime utilizando Catch2

---
+++ tests/test_primes_compiletime_catch2.cpp
@@ -0,0 +1,26 @@
+#define CATCH_CONFIG_MAIN
+#include <catch2/catch.hpp>
+#include "../include/core/internal/lookup_tables/primes_compiletime.hpp"
+
+TEST_CASE("Primalidad compile-time y runtime", "[primes]")
+{
+    // Compile-time (static_assert) - si falla, no compila
+    static_assert(NumRepr::AuxFunc::LUT::is_prime_lt_65537_ct<2>(), "2 debe ser primo");
+    static_assert(NumRepr::AuxFunc::LUT::is_prime_lt_65537_ct<3>(), "3 debe ser primo");
+    static_assert(NumRepr::AuxFunc::LUT::is_prime_lt_65537_ct<65521>(), "65521 debe ser primo");
+    static_assert(!NumRepr::AuxFunc::LUT::is_prime_lt_65537_ct<0>(), "0 no debe ser primo");
+    static_assert(!NumRepr::AuxFunc::LUT::is_prime_lt_65537_ct<1>(), "1 no debe ser primo");
+    static_assert(!NumRepr::AuxFunc::LUT::is_prime_lt_65537_ct<4>(), "4 no debe ser primo");
+    static_assert(!NumRepr::AuxFunc::LUT::is_prime_lt_65537_ct<65535>(), "65535 no debe ser primo");
+    static_assert(!NumRepr::AuxFunc::LUT::is_prime_lt_65537_ct<65536>(), "65536 no debe ser primo");
+
+    // Runtime
+    REQUIRE(NumRepr::AuxFunc::LUT::is_prime_lt_65537_ct<2>());
+    REQUIRE(NumRepr::AuxFunc::LUT::is_prime_lt_65537_ct<3>());
+    REQUIRE(NumRepr::AuxFunc::LUT::is_prime_lt_65537_ct<65521>());
+    REQUIRE_FALSE(NumRepr::AuxFunc::LUT::is_prime_lt_65537_ct<0>());
+    REQUIRE_FALSE(NumRepr::AuxFunc::LUT::is_prime_lt_65537_ct<1>());
+    REQUIRE_FALSE(NumRepr::AuxFunc::LUT::is_prime_lt_65537_ct<4>());
+    REQUIRE_FALSE(NumRepr::AuxFunc::LUT::is_prime_lt_65537_ct<65535>());
+    REQUIRE_FALSE(NumRepr::AuxFunc::LUT::is_prime_lt_65537_ct<65536>());
+}
